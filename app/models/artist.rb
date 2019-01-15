c0b3b2586ba572e3015b8be698c0001e0e91cbb8
EvanPavley <github email address>
2019-01-15 17:33:43 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,9 @@
 class Artist < ActiveRecord::Base
+  has_many :songs
+
+  # counts the songs
+  def song_count
+    self.songs.count
+  end
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,9 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  # gets the artist name
+  def artist_name
+    self.artist.name
+  end
+
 end
