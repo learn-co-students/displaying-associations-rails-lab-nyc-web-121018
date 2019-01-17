f7c59e292797ef79ca187a2eec76e5b7f1941f35
diaaanek <dkorongy@gmail.com>
2019-01-17 00:19:00 -0500

    Done.

--- db/migrate/20160108015311_create_songs.rb
+++ db/migrate/20160108015311_create_songs.rb
@@ -2,6 +2,8 @@ class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
       t.string :title
+      t.integer :artist_id
+      # references :artist
 
       t.timestamps null: false
     end
